a5e6680b4c97357989e679fea109852b6cc86b7c
Yura <yaxon@tut.by>
2019-01-06 00:38:50 +0300

    add prettier

--- server.js
+++ server.js
@@ -7,15 +7,21 @@ var bodyParser = require("body-parser");
 
 var app = express();
 const users = ["Andrey", "Sergey", "Yura"];
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.set("view engine", "pug");
 
-app.get("/", function(req, res) { res.render("index", { title: "server NodeJS", users: users });
+app.get("/", function (req, res) {
+  res.render("index", {
+    title: "server NodeJS",
+    users: users
+  });
 });
-app.get("/create", function(req, res) {
+app.get("/create", function (req, res) {
   res.render("create");
 });
-app.post("/create", function(req, res) {
+app.post("/create", function (req, res) {
   console.log(req.body);
   users.push(req.body.text);
   res.redirect("/");
