449708e5a316ef1787f1d75868ffd5a0815a680a
Yura <yaxon@tut.by>
2019-01-05 13:57:25 +0300

    add Prettier

--- server.js
+++ server.js
@@ -1,45 +1,25 @@
-var http = require('http');
-var url = require('url');
-var querystring = require('querystring');
-var static = require('node-static');
-var express = require('express');
+var http = require("http");
+var url = require("url");
+var querystring = require("querystring");
+var static = require("node-static");
+var express = require("express");
+var bodyParser = require("body-parser");
+
 var app = express();
+const users = ["Andrey", "Sergey", "Yura"];
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set("view engine", "pug");
 
-
-app.set('view engine', 'pug');
-//app.set('views', './views')
-
-/*
-var file = new static.Server('.', {
-  cache: 0
+app.get("/", function(req, res) {
+  res.render("index", { title: "server NodeJS", users: users });
+});
+app.get("/create", function(req, res) {
+  res.render("create");
+});
+app.post("/create", function(req, res) {console.log(req.body); users.push(req.body.text);
+  res.redirect("/");
 });
 
-function accept(req, res) {
-    console.log('пришел запрос-',req.url);
-    file.serve(req, res);
-}
-
-// ------ запустить сервер -------
-if (!module.parent) {
-    console.log('запуск сервера');
-  http.createServer(accept).listen(8081);
-  console.log('сервер запущен на порту 8081');
-} else {
-    console.log('сервер не запускается');
-  exports.accept = accept;
-}
-*/
-
-// express
- 
-// app.get('/', function (req, res) { res.send('Hello World') })
- 
-app.get('/', function (req, res) {
-  res.render('index', { title: 'Hey', message: 'Hello there!'});
+app.listen(3000, () => {console.log("сервер запущен на порту 3000");
 });
-app.get('/create', function (req, res) {
-  res.render('create');
-});
-
-app.listen(3000, () => {console.log('сервер запущен на порту 3000')});
-// app.use(express.static('.'));
+app.use(express.static("."));
