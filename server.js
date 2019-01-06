ac3c6158609689931f93067fe6c4c3cd8a52b789
Yura <yaxon@tut.by>
2019-01-07 00:15:59 +0300

    //

--- server.js
+++ server.js
@@ -4,30 +4,32 @@ var querystring = require("querystring");
 var static = require("node-static");
 var express = require("express");
 var bodyParser = require("body-parser");
-const config = require('./config');
+const config = require("./config");
 
 var app = express();
 
 const users = ["Andrey", "Sergey", "Yura"];
 
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
 
 app.set("view engine", "pug");
 
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.render("index", {
     title: "server NodeJS",
     users: users
   });
 });
 
-app.get("/create", function (req, res) {
+app.get("/create", function(req, res) {
   res.render("create");
 });
 
-app.post("/create", function (req, res) {
+app.post("/create", function(req, res) {
   console.log(req.body);
   users.push(req.body.text);
   res.redirect("/");
@@ -37,4 +39,4 @@ app.listen(config.PORT, () => {
   console.log(`сервер запущен на порту ${config.PORT}`);
 });
 
-app.use(express.static("."));
+app.use(express.static(".")); //
